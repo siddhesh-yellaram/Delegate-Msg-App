df3e70e6dec6cefe715894afeac46068f9cca7bb
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 01:02:42 +0530

    Add files via upload

---
+++ DelegateMsgApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateMsgApp
+{
+    class Program
+    {
+        delegate void DPrintMsg(string msg);
+        static void Main(string[] args)
+        {
+            DPrintMsg msg;
+            msg = PrintHello;
+            msg("Siddhesh");
+            msg += PrintHello;
+            msg += PrintGoodBye;
+            msg("Sid");
+            Console.ReadLine();
+        }
+
+        static void PrintHello(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+        static void PrintGoodBye(string name)
+        {
+            Console.WriteLine("Good Bye " + name);
+        }
+
+        static void foo()
+        {
+
+        }
+    }
+}
